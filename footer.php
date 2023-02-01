7896737910a7806e937939f7432cf07aca7cb261
MrSharma001 <110118837+MrSharma001@users.noreply.github.com>
2023-02-01 11:32:37 +0530

    frist commit

---
+++ footer.php
@@ -0,0 +1,4 @@
+<div class="container-fluid mt-5">
+    <hr>
+    <p class="text-center">Copyright IDiscuss Coding Forum 2022 | All Right Reserved</p>
+</div>
---
+++ sign_up.php
@@ -0,0 +1,38 @@
+
+<!-- Modal -->
+<div class="modal fade" id="sign_up_modal" tabindex="-1" aria-labelledby="sign_up_modal_title" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="sign_up_modal_title">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <form>
+                <div class="modal-body">
+
+                    <div class="mb-3">
+                        <label>User Name</label>
+                        <input type="text" class="form-control" name="userName" id="userName" aria-describedby="emailHelp">
+                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div class="mb-3">
+                        <label>Password</label>
+                        <input type="password" class="form-control" id="password" name="pass">
+                    </div>
+                    <div class="mb-3">
+                        <label>Password</label>
+                        <input type="password" class="form-control" id="cpass" name="cpass">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+
+                </div>
+            </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
